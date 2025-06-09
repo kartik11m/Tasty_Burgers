98ce0b55a393cd688a993c6680208b6535009d94
kartik11m <kartikmaheshwari698@gmail.com>
2025-06-09 11:49:32 +0530

    changing the directory of Main.jsx for the production

--- Main.jsx
+++ Main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import "./index.css";
-import App from "./App.jsx";
+import App from "./src/App.jsx";
 import {Provider} from "react-redux";
-import Store from "./redux/store.js";
+import Store from "./src/redux/store.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
